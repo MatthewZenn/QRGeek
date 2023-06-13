efb01a2557c3ad82fd4e1537e711f9c710baec52
Matthew Zenn <spyguy57@gmail.com>
2023-06-13 16:06:44 -0400

    initialized app

---
+++ index.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = "rgb(20, 20, 20)";
+const generate = document.getElementById("generate");
---
+++ main.js
@@ -0,0 +1,16 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({width: 1600, height: 900, resizable: false, frame:false, icon: "Assets/Animate.ico"});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+});
